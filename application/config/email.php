1d53ce32d0e61d20c363732055c38c28b3a269c5
Jesús Vielma <jesusvielma309@gmail.com>
2018-03-05 14:00:48 -0400

    Cambios

--- application/config/email.php
+++ application/config/email.php
@@ -1,11 +1,25 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$config = array(
-    'protocol' => 'smtp',
-    'smtp_host' => 'phx.hn.cl',
-    'smtp_port' => 26,
-    'smtp_user' => '_mainaccount@max-bread.cl',
-    'smtp_pass' => 'ptz8N6$-=,',
-    'crlf' => "\r\n",
-    'newline' => "\r\n",
-    'send_multipart' => false,
-);
+
+$conf = json_decode(get_site_config_val('correo'));
+if ($conf->smtp_host != '') {
+    $config = array(
+        'protocol' => 'smtp',
+        'smtp_host' => $conf->smtp_host,
+        'smtp_port' => $conf->smtp_port,
+        'smtp_user' => $conf->smtp_user,
+        'smtp_pass' => $conf->smtp_pass,
+        'crlf' => "\r\n",
+        'newline' => "\r\n",
+        'send_multipart' => false,
+        'smtp_crypto' => 'tls',
+        'useragent' => site_name().' - Email'
+    );
+}else{
+    $config = array(
+        'protocol' => 'mail',
+        'crlf' => "\r\n",
+        'newline' => "\r\n",
+        'send_multipart' => false,
+        'useragent' => site_name() . ' - Email'
+    );
+}
--- application/controllers/administrador/Perfil.php
+++ application/controllers/administrador/Perfil.php
@@ -123,19 +123,6 @@ class Perfil extends CI_Controller {
 	{
 		$this->load->library('email');
 
-		/* $config = array(
-			'protocol' => 'smtp',
-			'smtp_host' => 'phx.hn.cl',
-			'smtp_port' => 26,
-			'smtp_user' => '_mainaccount@max-bread.cl',
-			'smtp_pass' => 'concha.5283',
-			'crlf' => "\r\n",
-			'newline' => "\r\n",
-			'send_multipart' => false,
-		);
-
-		$this->email->initialize($config); */
-
 		$this->email->from('maxbread@max-bread.cl', 'Max Bread');
 		$this->email->to($_data['correo']);
 
